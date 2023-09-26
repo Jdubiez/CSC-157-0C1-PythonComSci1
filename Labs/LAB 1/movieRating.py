182c8d00b16975d520bc68d109e3fc250946c1cb
Jdubiez <120103384+Jdubiez@users.noreply.github.com>
2023-09-26 08:50:01 +0000

    yurr

--- Labs/LAB 1/movieRating.py
+++ Labs/LAB 1/movieRating.py
@@ -1,6 +1,6 @@
-
 import math
-'''
+
+"""
 Sample Run:
 Please enter the movie name.
 CSC 155 The Movie!
@@ -21,21 +21,26 @@ Average website rating: 61.33
 Average focus group rating: 88.40
 Average movie critic rating: 87.58
 Overall movie rating: 83
-'''
+"""
 
-movieTitle = 'Title: ' + str(input('Please enter the movie name.\n'))
+movieTitle = "Title: " + str(input("Please enter the movie name.\n"))
 print(movieTitle)
 
-movieRunTime = int(input('Please enter the running time in minutes.\n'))
+movieRunTime = int(input("Please enter the running time in minutes.\n"))
 
-runTimeHour=  'Running time: '+ str(math.floor(movieRunTime/60)) + 'h' + str((movieRunTime%60))
+runTimeHour = (
+    "Running time: "
+    + str(math.floor(movieRunTime / 60))
+    + "h"
+    + str((movieRunTime % 60))
+)
 print(runTimeHour)
 
 
-movieReviewRatings =float(input
-    ('Please enter ratings from the movie review webstite\n')) + float(input()) + float(input())
+movieReviewRatings = (
+    float(input("Please enter ratings from the movie review webstite\n"))
+    + float(input())
+    + float(input())
+)
 
-movieReviewRatingsAVG = print("{:.1f}".format(movieReviewRatings/3))
-
-
-
+movieReviewRatingsAVG = print("{:.1f}".format(movieReviewRatings / 3))
