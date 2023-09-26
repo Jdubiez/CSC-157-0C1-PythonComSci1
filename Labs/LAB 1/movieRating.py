eafee649804e1e4e9800f927e1702ebbcb6ffa60
Jdubiez <120103384+Jdubiez@users.noreply.github.com>
2023-09-26 08:17:33 +0000

    yurr

--- Labs/LAB 1/movieRating.py
+++ Labs/LAB 1/movieRating.py
@@ -1,5 +1,27 @@
 
 import math
+'''
+Sample Run:
+Please enter the movie name.
+CSC 155 The Movie!
+Please enter the running time in minutes.
+135
+Please enter ratings from the movie review website.
+75
+99
+10
+Please enter ratings from the focus group.
+84.5
+92.3
+Please enter the average movie critic rating.
+87.58
+Title: CSC 155 The Movie!
+Running time: 2h15
+Average website rating: 61.33
+Average focus group rating: 88.40
+Average movie critic rating: 87.58
+Overall movie rating: 83
+'''
 
 movieTitle = 'Title: ' + str(input('Please enter the movie name.\n'))
 print(movieTitle)
